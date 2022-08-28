4f6575e5da92792d284b2d387b5627323b812094
sahilkumarbarick <91127214+sahilkumarbarick@users.noreply.github.com>
2022-08-28 23:21:45 +0530

    Add files via upload

---
+++ app/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+/**this is Data Access Object interface where database operations are done*/
+@Dao
+interface SleepDatabaseDao{
+    @Insert
+    suspend fun insert(night: SleepNight)
+
+    @Update
+    suspend fun update(night: SleepNight)
+
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE nightId = :key")
+    suspend fun get(key:Long):SleepNight?
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    suspend fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    suspend fun getTonight():SleepNight?
+
+    /**
+     * Room already uses a background thread for that specific @Query which returns LiveData
+       so there's no need to add suspend keyword.
+     */
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights():LiveData<List<SleepNight>>
+
+    /**
+     * Selects and returns the night with given night id
+     */
+    @Query("Select * from daily_sleep_quality_table WHERE nightId = :key")
+    fun getNightWithId(key: Long) :LiveData<SleepNight>
+}
